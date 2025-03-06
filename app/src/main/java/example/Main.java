def30985b166b8420baf1a9d88a08ca902699d15
kuye <kunlin.ye@student.unimelb.edu.au>
2025-03-06 11:01:23 +1100

    123

--- app/src/main/java/example/Main.java
+++ app/src/main/java/example/Main.java
@@ -4,8 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("add = " + calculator.add(3, 5));
+
+        //1234567899
         System.out.println("minus = " + calculator.minus(8, 2));
+
+
         System.out.println("multiply = " + calculator.multiply(3, 5));
+
+
         System.out.println("divide = " + calculator.divide(6, 3));
 
     }
